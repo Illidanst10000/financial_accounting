9bbb964e74dbaab50cdd8e47cbabb6dcfc38555d
illidanst10000 <illidanst10000@gmail.com>
2022-05-12 16:29:41 +0300

    Create migration

---
+++ app/Models/Type.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Type extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'types';
+    protected $guarded = false;
+
+    public function earnings() {
+        return $this->hasMany(Earning::class);
+    }
+
+    public function spendings() {
+        return $this->hasMany(Spending::class);
+    }
+}
